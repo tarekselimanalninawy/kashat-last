e49b62bf650b9aaff1a7d933d59b76169b8c9171
TarekSeliman2020 <tarek.seliman2010@gmail.com>
2021-08-16 06:51:16 +0200

    creat project

---
+++ form-handler.php
@@ -0,0 +1,22 @@
+<?php 
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$emai_form ='info@yourwebsite.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+"User Email: $visitor_email.\n"
+"Subject: $subject.\n"
+"User Message: $message.\n";
+$to = 'moatasem.hatem@kashat.com.eg';
+$headers = "From" :$emai_form \r\n;
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header ("Location: contact.html");
+?>
